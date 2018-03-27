143fe8ed8de15f0bd67d906d513df45c5cfdd5d9
erealmsoft <dev@erealmsoft.com>
2018-03-27 17:19:34 +0800

    remove default icons and add WF icons.

--- WFIcons.js
+++ WFIcons.js
@@ -1,12 +1,12 @@
 /**
- * Foundation icon set component.
- * Usage: <Foundation name="icon-name" size={20} color="#4F8EF7" />
+ * Feather icon set component.
+ * Usage: <Feather name="icon-name" size={20} color="#4F8EF7" />
  */
 
 import createIconSet from './lib/create-icon-set';
-import glyphMap from './glyphmaps/Foundation.json';
+import glyphMap from './glyphmaps/WF.json';
 
-const iconSet = createIconSet(glyphMap, 'fontcustom', 'Foundation.ttf');
+const iconSet = createIconSet(glyphMap, 'WFIcons', 'WF.ttf');
 
 export default iconSet;
 
